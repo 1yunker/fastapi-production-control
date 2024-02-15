4bae15b57229b7fe19663ca9f22270f649e9c008
Yuri Pomazkin <1yunker@gmail.com>
2024-02-15 13:30:18 +0500

    feat: Added alembic migrations, updated schemas

--- src/schemas/batch.py
+++ src/schemas/batch.py
@@ -1,7 +1,43 @@
-from pydantic import BaseModel, ConfigDict
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel, ConfigDict, Field
 
 
 class BatchRequest(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
-    pass
+    is_closed: bool = Field(..., alias="СтатусЗакрытия")
+    description: str = Field(..., alias="ПредставлениеЗаданияНаСмену")
+    work_center: str = Field(..., alias="Линия")
+    shift: str = Field(..., alias="Смена")
+
+    squad: str = Field(..., alias="Бригада")
+    number: int = Field(..., alias="НомерПартии")
+    date: datetime.date = Field(..., alias="ДатаПартии")
+    nomenclature: str = Field(..., alias="Номенклатура")
+
+    code_ekn: str = Field(..., alias="КодЕКН")
+    work_center_id: str = Field(..., alias="ИдентификаторРЦ")
+    start_date: datetime.datetime = Field(..., alias="ДатаВремяНачалаСмены")
+    end_date: datetime.datetime = Field(..., alias="ДатаВремяОкончанияСмены")
+
+
+class BatchResponse(BaseModel):
+    id: int
+    description: str
+    work_center: str
+    shift: str
+
+    squad: str
+    number: int
+    date: datetime.date
+    nomenclature: str
+
+    code_ekn: str
+    work_center_id: str
+    start_date: datetime.datetime
+    end_date: datetime.datetime
+
+    is_closed: bool
+    closed_at: Optional[datetime.datetime]
