74886430fa255d39e74523e787904b5cc3e0e4fb
Yuri Pomazkin <1yunker@gmail.com>
2024-02-15 22:47:06 +0500

    fix: Updated requirements and imports

--- src/db/db.py
+++ src/db/db.py
@@ -2,6 +2,7 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
 from core.config import app_settings
+from models.models import Base 
 
 engine = create_async_engine(
     str(app_settings.database_dsn),
@@ -19,3 +20,11 @@ async_session = sessionmaker(
 async def get_session() -> AsyncSession:
     async with async_session() as session:
         yield session
+
+
+async def recreate_tables_in_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+    await engine.dispose()
--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
@@ -5,6 +7,7 @@ from fastapi.responses import ORJSONResponse
 from api.v1 import base
 from core import logger
 from core.config import app_settings
+from db.db import recreate_tables_in_db
 
 app = FastAPI(
     title=app_settings.app_title,
@@ -14,6 +17,7 @@ app.include_router(base.router)
 
 
 if __name__ == '__main__':
+    asyncio.run(recreate_tables_in_db())
     uvicorn.run(
         'main:app',
         host=app_settings.project_host,
