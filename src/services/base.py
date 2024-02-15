61c9eca0068a02ed49420afbb59b1dfd25f70e42
Yuri Pomazkin <1yunker@gmail.com>
2024-02-15 14:10:14 +0500

    feat: Added base crud operations

---
+++ src/services/base.py
@@ -0,0 +1,84 @@
+from typing import Any, Dict, Generic, List, Optional, Type, TypeVar, Union
+
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
+from sqlalchemy import select, update
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from models.models import Base
+
+ModelType = TypeVar("ModelType", bound=Base)
+CreateSchemaType = TypeVar("CreateSchemaType", bound=BaseModel)
+UpdateSchemaType = TypeVar("UpdateSchemaType", bound=BaseModel)
+
+
+class Repository:
+
+    def get(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def get_multi(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def create(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def update(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def delete(self, *args, **kwargs):
+        raise NotImplementedError
+
+
+class RepositoryDB(
+    Repository,
+    Generic[ModelType, CreateSchemaType, UpdateSchemaType]
+):
+    def __init__(self, model: Type[ModelType]):
+        self._model = model
+
+    async def get(
+            self, db: AsyncSession, id: Any
+    ) -> Optional[ModelType]:
+        statement = select(self._model).where(self._model.id == id)
+        results = await db.execute(statement=statement)
+        return results.scalar_one_or_none()
+
+    async def get_multi(
+            self, db: AsyncSession, *, skip=0, limit=100
+    ) -> List[ModelType]:
+        statement = select(self._model).offset(skip).limit(limit)
+        results = await db.execute(statement=statement)
+        return results.scalars().all()
+
+    async def create(
+            self, db: AsyncSession, *, obj_in: CreateSchemaType
+    ) -> ModelType:
+        obj_in_data = jsonable_encoder(obj_in)
+        db_obj = self._model(**obj_in_data)
+        db.add(db_obj)
+        await db.commit()
+        await db.refresh(db_obj)
+        return db_obj
+
+    async def update(
+            self,
+            db: AsyncSession,
+            *,
+            db_obj: ModelType,
+            obj_in: Union[UpdateSchemaType, Dict[str, Any]]
+    ) -> ModelType:
+        statement = (
+            update(self._model).
+            where(self._model.id == db_obj.id).
+            values(obj_in.model_dump(exclude_unset=True)).
+            returning(self._model)
+        )
+        await db.execute(statement=statement)
+        await db.commit()
+        return db_obj
+
+    async def delete(self, db: AsyncSession, *, id: int) -> ModelType:
+        db_obj = db.get(self._model, id)
+        await db.delete(db_obj)
+        return db_obj
